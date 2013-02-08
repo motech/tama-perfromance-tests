68a7baf4bf51b76749c413e799a785b274304cb7
Balaji Narain <balajin@thoughtworks.com>
2013-02-08 15:23:42 +0530

    Shashank,BalajiN | #0 | Setting up data for TAMA performance tests using benerator

--- src/test/java/org/motechproject/tama/performance/patient/PatientTest.java
+++ src/test/java/org/motechproject/tama/performance/patient/PatientTest.java
@@ -10,6 +10,9 @@ public class PatientTest {
 
     @Test
     public void createPatient() throws IOException {
+        long startTime = System.currentTimeMillis();
         Benerator.main(new String[]{"patient_registration.ben.xml"});
+        long endTime = System.currentTimeMillis();
+        System.out.println("Timetaken: " + (endTime - startTime)/1000 + " seconds");
     }
 }
